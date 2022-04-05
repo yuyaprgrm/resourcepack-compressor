8b73b6c9e4a5f48a13fee390aa69ab740aee50be
famima65536 <famima65536@gmail.com>
2022-04-05 10:15:53 +0900

    fix typo

--- src/compressor.py
+++ src/compressor.py
@@ -3,7 +3,7 @@ import shutil
 import os
 
 pack_src = os.environ['PACK_SRC']
-pack_dist = os.environ['PACK_DIST']
+pack_dest = os.environ['PACK_DEST']
 pack_name = os.environ['PACK_NAME']
 workspace = 'resourcepack_workspace'
 
@@ -14,10 +14,10 @@ rule = shutil.ignore_patterns(".*")
 shutil.copytree(pack_src, workspace, ignore=rule)
 
 # make zip archive
-shutil.make_archive(pack_dist, 'zip', workspace)
+shutil.make_archive(pack_dest, 'zip', workspace)
 
 file_name_zip = f"{pack_name}.zip"
 file_name_mcpack = f"{pack_name}.mcpack"
 
 # rename zip to 
-os.rename(os.path.join(pack_dist, file_name_zip), os.path.join(pack_dist, file_name_mcpack))
+os.rename(os.path.join(pack_dest, file_name_zip), os.path.join(pack_dist, file_name_mcpack))
